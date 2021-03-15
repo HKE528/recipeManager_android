59e3c60ff8f0f8982596dfd4fd46feb8a9e71f0d
HKE <gosumo12@gmail.com>
2021-03-15 22:00:08 +0900

    툴바 검색, 더보기 메뉴 추가

--- recipeManager/app/src/main/java/com/example/recipemanager/MainActivity.kt
+++ recipeManager/app/src/main/java/com/example/recipemanager/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.recipemanager
 
+import android.app.TaskStackBuilder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(layout_toolbar as androidx.appcompat.widget.Toolbar)
+        supportActionBar?.setDisplayShowCustomEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onCreateNavigateUpTaskStack(builder: TaskStackBuilder?) {
+        
+        super.onCreateNavigateUpTaskStack(builder)
+    }
+    
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        
+        menuInflater.inflate(R.menu.toolbar_actions, menu)
+        
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //툴바 엑션 처리
+        return super.onOptionsItemSelected(item)
     }
 }
+
